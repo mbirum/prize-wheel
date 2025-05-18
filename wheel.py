f5248e01d2fe8423314d3a69465a9829dac92166
Matt Birum <45514707+mbirum@users.noreply.github.com>
2025-05-18 18:51:12 -0400

    Create wheel.py

---
+++ wheel.py
@@ -0,0 +1,77 @@
+from RPi import GPIO
+from time import sleep
+import sys
+import os
+import led
+
+channel = sys.argv[1]
+clkPin = int(sys.argv[2])
+dtPin = int(sys.argv[3])
+swPin = int(sys.argv[4])
+knobIncrement = int(sys.argv[5])
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(clkPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dtPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(swPin, GPIO.IN)
+
+min = 0
+max = 127
+pos = min
+clkLast = GPIO.input(clkPin)
+dtLast = GPIO.input(dtPin)
+# swLast = GPIO.input(swPin)
+posLast = pos
+
+try:
+
+    # led.powerOn()
+
+    increment = 0.0001
+  
+    # counter = 0.0
+    # while True:
+	   #  c = int(counter * 10000)
+	   #  if 100 == c:
+		  #   counter = 0.0
+		  #   sw = GPIO.input(swPin)
+		  #   if sw != swLast:
+			 #    if sw == GPIO.LOW:
+				#     print "Pressed!"
+		    swLast = sw
+
+	clk = GPIO.input(clkPin)
+  dt = GPIO.input(dtPin)
+  full = True
+
+  #half or full click
+  if clk != dt:
+      full = False
+      
+  if full:
+      #left click
+      if clkLast != clk and dtLast == dt:
+          pos -= knobIncrement
+          if pos < min:
+              pos = max # loop back
+      #right click
+      elif dtLast != dt and clkLast == clk:
+          pos += knobIncrement
+          if pos > max:
+              pos = min # loop back
+
+      if posLast != pos:
+          # do something with pos
+          print(pos)
+
+  clkLast = clk
+  dtLast = dt
+
+  posLast = pos
+  sleep(increment)
+
+finally:
+    #os.system('/home/pi/devl/midi/midichan reset %s'%(channel))
+    GPIO.cleanup()
+    print "done"
