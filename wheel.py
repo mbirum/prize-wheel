33f44a83b11b0b9052302acc8cbe9c955fc22c59
Matt Birum <45514707+mbirum@users.noreply.github.com>
2025-05-19 13:48:43 -0400

    Update wheel.py

--- wheel.py
+++ wheel.py
@@ -1,5 +1,6 @@
 from RPi import GPIO
 from time import sleep
+import time
 import sys
 import os
 # import led
@@ -15,14 +16,18 @@ GPIO.setup(clk_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 min = 0
-max = 127
+max = 1000000
 position = min
 clk_last = GPIO.input(clk_pin)
 dt_last = GPIO.input(dt_pin)
 position_last = position
 
 try:
+	potential_spin = False
+	potential_spin_position = position
 	spin_started = False
+	movement_start_time = None
+	spin_start_time = None
 	
 	while True:
 		clk = GPIO.input(clk_pin)
@@ -47,7 +52,18 @@ try:
 	
 		if position_last != position:
 			if not spin_started:
-				spin_started = True
+				if not potential_spin:
+					potential_spin = True
+					movement_start_time = int(time.time())
+					potential_spin_position = position
+				else:
+					current_time = int(time.time())
+					time_diff = int((current_time - movement_start_time) / 1000) # in seconds
+					position_diff = position - potential_spin_position
+					if time_diff >= 1:
+						if position_diff >= 10:
+							spin_started = True
+							potential_spin = False
 		else:
 			if spin_started:
 				spin_started = False
@@ -58,7 +74,7 @@ try:
 	
 		position_last = position
 		
-		sleep_interval = 0.01
+		sleep_interval = 0.0001
 		sleep(sleep_interval)
 
 finally:
