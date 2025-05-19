b3596aa76ed1ed3ee3fd6ea027d8e86db5c7cfad
Matt Birum <45514707+mbirum@users.noreply.github.com>
2025-05-19 10:47:12 -0400

    Update get-wheel-input.sh

--- .github/workflows/scripts/get-wheel-input.sh
+++ .github/workflows/scripts/get-wheel-input.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-echo "Hello"
+ 
+approval=$(gh api -H "Accept: application/vnd.github+json" "/repos/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}/approvals")
+comment="$(jq -r '.[0].comment' <<< "$approval")"
+echo "item_number=$comment" >> "$GITHUB_OUTPUT"
