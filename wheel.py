892319554fc80cb27c92fe5914f274fb84fd4066
Matt Birum <45514707+mbirum@users.noreply.github.com>
2025-10-11 17:01:49 -0400

    Update wheel.py

--- wheel.py
+++ wheel.py
@@ -2,8 +2,12 @@ import time
 import board
 import busio
 import adafruit_mpr121
+from time import sleep
+
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
+
+increment = 0.01
 
 while True:
     for i in range(12):
@@ -11,3 +15,4 @@ while True:
             print('Input {} touched!'.format(i))
         else:
             print('|')
+    sleep(increment)
