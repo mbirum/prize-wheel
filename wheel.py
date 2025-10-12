89e15e599509fd6f99fc71aeb7ab50b9bfd4ddf6
Matt Birum <birum18@gmail.com>
2025-10-11 20:33:28 -0400

    sense spin

--- wheel.py
+++ wheel.py
@@ -4,18 +4,44 @@ import busio
 import adafruit_mpr121
 from time import sleep
 
+# initialize sensor
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
-increment = 0.06
+# initialize input buffer
 input_buffer = []
-buffer_size = 15
+buffer_size = 10
+for i in range(buffer_size):
+    input_buffer[i] = False
+
+interval = 0.06
 target_pin = 8
+attempt_started = False
+
+# started if buffer is all 'True'
+def has_attempt_started:
+    result = True
+    for i in range(buffer_size):
+        result = result and i
+    return result
 
 while True:
-    print(f'{mpr121[target_pin].value}')
-    # if mpr121[target_pin].value:
-    #     print('Input {} touched!'.format(target_pin))
-    # else:
-    #     print('|')
-    sleep(increment)
+    # print(f'{mpr121[target_pin].value}')
+    value = mpr121[target_pin].value
+    input_buffer.pop(0)
+    input_buffer.append(value)
+
+    if value:
+        if has_attempt_started():
+            attempt_started = True
+            print('SPIN!')
+    else:
+        if attempt_started:
+            attempt_started = False
+            print('stopped...')
+
+    sleep(interval)
+
+
+
+
